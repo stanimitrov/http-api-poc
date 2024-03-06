f878c4c52271090de62788df978e3e4916569e03
stanimitrov <stanimir@telebid-pro.com>
2024-03-06 17:27:48 +0200

    feat: put endpoint

--- todosRouter.js
+++ todosRouter.js
@@ -82,6 +82,48 @@ todosRouter.delete("/todos/:id", async (req, res) => {
   }
 });
 
+todosRouter.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const { title, description } = req.body;
+
+  if (!id || !title || !description) {
+    return res.status(400).json({
+      error: "The 'id', 'title', and 'description' fields must be provided!",
+    });
+  }
+
+  if (isNaN(id)) {
+    return res.status(400).json({
+      error: "The 'id' field must a number!",
+    });
+  }
+
+  try {
+    const selectResult = await db.query(
+      `SELECT * from ${TODOS_TABLE} Where id = $1`,
+      [id]
+    );
+
+    if (!selectResult.rows.length) {
+      return res
+        .status(404)
+        .json({ error: "A todo with id: '" + id + "' was not found" });
+    }
+
+    const updateResult = await db.query(
+      `Update ${TODOS_TABLE} SET title = $1, description = $2 WHERE id = $3 RETURNING id`,
+      [title, description, id]
+    );
+
+    return res.status(200).json({ result: updateResult.rows[0] });
+  } catch (err) {
+    return res.status(500).json({
+      error: "Something went wrong with updating the todo. " + err,
+    });
+  }
+});
+
 module.exports = {
   todosRouter,
 };
