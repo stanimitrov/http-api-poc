c0d977797eb4b2f874cec0ee8622d75fd780ee20
stanimitrov <stanimir@telebid-pro.com>
2024-03-06 14:16:57 +0200

    refactor: remove app use

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
@@ -7,8 +7,6 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use(() => {
-  app.listen(SERVER_PORT, () => {
-    console.log("App running on port: " + SERVER_PORT);
-  });
+app.listen(SERVER_PORT, () => {
+  console.log("App running on port: " + SERVER_PORT);
 });
