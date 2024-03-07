7b44892d3df633b6904e89bdf33fd98b77c537e6
stanimitrov <stanimir@telebid-pro.com>
2024-03-07 16:58:20 +0200

    feat: create the Create Todo modal

--- frontend/script.js
+++ frontend/script.js
@@ -2,4 +2,8 @@ import { renderTodos } from "./core/todos/renderTodos.js";
 
 $(() => {
   renderTodos();
+
+  $(".create-todo-btn").on("click", () => {
+    $("#create-todo-modal").show();
+  });
 });
