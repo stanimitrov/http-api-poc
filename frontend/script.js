b806b000471300cb72a0b1a0490b24505d211480
stanimitrov <stanimir@telebid-pro.com>
2024-03-07 14:17:06 +0200

    feat: get all & single todos

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,33 @@
-$(document).ready(() => {});
+const getAllTodos = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/todos");
+
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getTodo = async (id) => {
+  try {
+    const response = await fetch(`http://localhost:3000/todos/${id}`);
+
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const organizeTodosInArray = async () => {
+  const { result: idsOfTodos } = await getAllTodos();
+
+  const todosPromiseMap = idsOfTodos.map(async (id) => {
+    return await getTodo(id);
+  });
+
+  return (await Promise.all(todosPromiseMap)).map((v) => v.result);
+};
+
+$(() => {
+  organizeTodosInArray().then((res) => console.log(res));
+});
