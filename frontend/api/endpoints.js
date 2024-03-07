14010888d9d919a99e59060b03db89de06b773eb
stanimitrov <stanimir@telebid-pro.com>
2024-03-07 18:52:04 +0200

    wip: create todo functionality

--- frontend/api/endpoints.js
+++ frontend/api/endpoints.js
@@ -2,5 +2,6 @@ export const endpoints = {
   todos: {
     getAll: "http://localhost:3000/todos",
     getOne: (id) => `http://localhost:3000/todos/${id}`,
+    create: "http://localhost:3000/todos",
   },
 };
--- frontend/script.js
+++ frontend/script.js
@@ -1,9 +1,66 @@
+import { createTodo } from "./api/todos/createTodo.js";
 import { renderTodos } from "./core/todos/renderTodos.js";
 
 $(() => {
+  // Render all todos from the DB upon page load
   renderTodos();
 
-  $(".create-todo-btn").on("click", () => {
+  // Display the modal upon "Create Todo" button click
+  $("#create-todo-btn").on("click", () => {
     $("#create-todo-modal").show();
+  });
+
+  // Enable the "Create" button in the "Create Todo" modal
+  $("#todo-description-input, #todo-title-input").on("keyup", () => {
+    const todoTitleInput = $("#todo-title-input").val();
+    const todoDescriptionInput = $("#todo-description-input").val();
+
+    if (
+      typeof todoTitleInput !== "object" &&
+      typeof todoDescriptionInput !== "object"
+    ) {
+      if (todoTitleInput && todoDescriptionInput) {
+        $("#submit-create-todo-btn").removeAttr("disabled");
+      }
+    }
+  });
+
+  // Handle the "Create" (submit) button click in the "Create Todo" modal
+  $("#submit-create-todo-btn").on("click", async () => {
+    const todoTitleInput = $("#todo-title-input");
+    const todoDescriptionInput = $("#todo-description-input");
+
+    try {
+      const res = await createTodo(
+        todoTitleInput.val(),
+        todoDescriptionInput.val()
+      );
+
+      if (!res.ok) {
+        $("<span>")
+          .text(
+            "Ooops! A todo with this title and/or description already exists "
+          )
+          .attr("id", "create-todo-error-message")
+          .addClass("text-danger mt-2")
+          .appendTo("#create-todo-modal-body");
+
+        return;
+      }
+
+      // Hide the modal from the UI if the request was successful
+      $(".modal-backdrop").remove();
+      $("#create-todo-modal").toggle();
+      $("body").addClass("overflow-auto");
+
+      // Clean the input fields in the modal
+      todoTitleInput.val("");
+      todoDescriptionInput.val("");
+
+      // Remove the error message from the modal if it was there
+      $("#create-todo-error-message").remove();
+    } catch (err) {
+      console.error(err);
+    }
   });
 });
